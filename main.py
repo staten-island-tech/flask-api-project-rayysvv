0528136a3a1208f777bc441eee12dfd4ef9c220d
rayysvv <rayysvv@gmail.com>
2025-04-10 10:17:43 -0400

    d

--- main.py
+++ main.py
@@ -1,9 +1,57 @@
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
+from flask import Flask, request, redirect, session, url_for
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv("CLIENT_SECRET")
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.cache_handler import FlaskSessionCacheHandler
 
-print(client_id, client_secret)
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(64)
+ 
+client_id = '74f8dddf62054a39819dd2a47ee5d861'
+client_secret = '9473112e421b46dcaebbb8142adba05c'
+redirect_uri = 'http://localhost:5000/callback'
+scope = 'playlist-read-private'
+
+cache_handler = FlaskSessionCacheHandler(session)
+sp_oauth = SpotifyOAuth(
+    client_id=client_id, 
+    client_secret=client_secret, 
+    redirect_uri=redirect_uri, 
+    scope=scope, 
+    cache_handler=cache_handler,
+    show_dialog=True
+)
+sp  = Spotify(auth_manager=sp_oauth)
+
+@app.route('/')
+def home():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    return redirect(url_for('get_playlists'))
+
+@app.route('/callback')
+def callback():
+    sp_oauth.get_access_token(request.args['code'])
+    return redirect(url_for('get_playlists'))
+
+@app.route('/get_playlists')
+def get_playlists():
+    if not sp_oauth.validate_token(cache_handler.get_cached_token()):
+        auth_url = sp_oauth.get_authorize_url()
+        return redirect(auth_url)
+    
+    playlists = sp.current_user_playlists()
+    playlist_info = [(pl['name'], pl['external_urls']['spotify']) for pl in playlist['items']]
+    playlist_html = '<br>'.join([f'{name}: {url}' for name, url in playlists_info])
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('home'))
+
+    return playlists_display
+if __name__ == "__main__":
+    app.run(debug=True)
